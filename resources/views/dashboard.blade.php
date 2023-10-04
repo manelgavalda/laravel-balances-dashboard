07fc303a3563913f2f62f3021eb1414f826c60dd
manel <manelgavalda1@gmail.com>
2023-10-05 01:00:38 +0200

    Add breeze authentication, remove welcome page and show basic graph

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="max-w-4xl">
+        <canvas id="chart" width="3" height="1"></canvas>
+    </div>
+    <script>
+        const ctx = document.getElementById('chart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+</x-app-layout>
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\User;
+
 it('returns a successful response', function () {
+    $this->actingAs(
+        User::factory()->create()
+    );
+
     $response = $this->get('/');
 
     $response->assertStatus(200);
