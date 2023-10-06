969b9952eebf0ea9df41c4f331553b2d3fbfd0e3
manel <manelgavalda1@gmail.com>
2023-10-06 23:01:58 +0200

    Style

--- resources/views/components/dashboard/chart.blade.php
+++ resources/views/components/dashboard/chart.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col col-span-full sm:col-span-12 bg-white dark:bg-slate-800 shadow-lg border border-slate-200 dark:border-slate-700">
-    <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700 flex items-center">
+    <header class="px-5 py-2 border-b border-slate-100 dark:border-slate-700 flex items-center">
         <h2 class="font-semibold text-slate-800 dark:text-slate-100">{{ $label }}</h2>
     </header>
     <div class="px-5 py-3">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
                 :dates="$balances['dates']"
                 color='blue'
                 label='Total ETH'
-                height="201"
+                height="193"
             />
         </div>
         <div class="w-1/3">
@@ -48,12 +48,12 @@
             />
         </div>
     </div>
-    <div class="col-span-full xl:col-span-12 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
-        <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+    <div class="col-span-full xl:col-span-12 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700 mt-2">
+        <header class="px-5 py-2 border-b border-slate-100 dark:border-slate-700">
             <h2 class="font-semibold text-slate-800 dark:text-slate-100">Balances</h2>
         </header>
         <!-- Table -->
-        <table class="table-autodark:text-slate-300 mx-auto mt-2 w-full">
+        <table class="table-autodark:text-slate-300 mx-auto w-full">
             <!-- Table header -->
             <thead class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
                 <tr>
@@ -90,11 +90,7 @@
                     <tr>
                         <td class="p-2 w-1/3">
                             <div class="flex items-center">
-                                <svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
-                                    <title>Ethereum icon</title>
-                                    <path d="M11.944 17.97L4.58 13.62 11.943 24l7.37-10.38-7.372 4.35h.003zM12.056 0L4.69 12.223l7.365 4.354 7.365-4.35L12.056 0z"/>
-                                </svg>
-                                <div class="text-slate-800 dark:text-slate-100 pl-1">{{ $token->pool }}</div>
+                                <div class="text-slate-800 dark:text-slate-100 pl-3">{{ $token->pool }}</div>
                             </div>
                         </td>
                         <td class="p-2">
