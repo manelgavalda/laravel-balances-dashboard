1521b414bfc2f14a5ae5002768ea316e36fb5023
manel <manelgavalda1@gmail.com>
2023-11-09 20:55:48 +0100

    Remove ethereum price from navigation

--- app/Livewire/Tokens.php
+++ app/Livewire/Tokens.php
@@ -25,13 +25,18 @@ class Tokens extends Component
     #[On('tokens-loaded')]
     public function reloadTokens()
     {
-        $tokens = Http::get("https://nuxt-test-tau-eosin.vercel.app/api/balances?token=M9yrAdZIxvwQM2Lq9OxCixzh!QSpL=sqYF!ocTnw9OS6VRITwooEiCrSuc!-CVVd")->object();
+        $result = Http::get("https://nuxt-test-tau-eosin.vercel.app/api/balances?token=M9yrAdZIxvwQM2Lq9OxCixzh!QSpL=sqYF!ocTnw9OS6VRITwooEiCrSuc!-CVVd")->object();
+
+        $tokens = $result->balances;
 
         $this->tokens->prepend(
             collect($tokens)->sortBy(fn ($token) => $token->price * $token->balance)
         );
 
+        $this->balances['prices'][] = $result->ethereumPrice->usd;
+        $this->balances['prices_eur'][] = $result->ethereumPrice->eur;
         $this->balances['totals'][] = collect($tokens)->sum(fn ($token) => $token->price * $token->balance);
         $this->balances['totals_eur'][] = collect($tokens)->sum(fn ($token) => $token->price_eur * $token->balance);
+        $this->balances['ethereum'][] = collect($tokens)->sum(fn ($token) => $token->price * $token->balance / $result->ethereumPrice->usd);
     }
 }
