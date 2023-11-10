b5902d4a2c834653ed6a6291d6aeea467aaaeae7
manel <manelgavalda1@gmail.com>
2023-11-10 16:24:23 +0100

    add tests for Tokens component

--- tests/Feature/Livewire/TokensTest.php
+++ tests/Feature/Livewire/TokensTest.php
@@ -3,11 +3,12 @@
 use App\Models\User;
 use Livewire\Livewire;
 use App\Livewire\Tokens;
+use App\Services\SupabaseService;
 
 beforeEach(function () {
     config([
         'wise.profile_id' => 'fake_profile_id',
-        'supabase.url' => 'https://fake-url.supabase.co'
+        'supabase.url' => 'https://fake-url.supabase.co',
     ]);
 
     fakeRequest('https://api.transferwise.com/v1/profiles/fake_profile_id/activities', 'latest_transactions');
@@ -17,16 +18,55 @@ beforeEach(function () {
 });
 
 it('exists_on_the_page', function() {
-    $user = User::factory()->create();
+    $this->actingAs(
+        User::factory()->create()
+    );
 
-    $this->actingAs($user);
-
-    $this->get('/')
-         ->assertSeeLivewire(Tokens::class);
+    $this->get('/')->assertSeeLivewire(Tokens::class);
 });
 
-it('tokens_are_correctly_set', function () {
+it('dispatches_an_event_when_tokens_are_loaded', function () {
+    $supabaseService = new SupabaseService('fake-api-key', 'https://fake-url.supabase.co');
+
     Livewire::test(Tokens::class)
-        ->assertViewHas('tokens')
-        ->assertDispatched('tokens-loaded');
+        ->assertDispatched('tokens-loaded')
+        ->assertSet('tokens', $supabaseService->getTokens())
+        ->assertSet('balances', $supabaseService->getHistoricalBalances());
+});
+
+it('tokens_are_refreshed_when_the_event_is_called', function () {
+    config(['tokens.api_url' => 'https://fake-tokens-url.com']);
+
+    fakeRequest('https://fake-tokens-url.com', 'new_tokens');
+
+    Livewire::test(Tokens::class)
+        ->assertViewHas('tokens', function ($tokens) {
+            expect(count($tokens))->toBe(30);
+
+            return true;
+        })
+        ->assertViewHas('balances', function ($balances) {
+            expect(count($balances['prices']))->toBe(31);
+            expect(count($balances['totals']))->toBe(31);
+            expect(count($balances['ethereum']))->toBe(31);
+            expect(count($balances['prices_eur']))->toBe(31);
+            expect(count($balances['totals_eur']))->toBe(31);
+
+            return true;
+        })
+        ->dispatch('tokens-loaded')
+        ->assertViewHas('tokens', function ($tokens) {
+            expect(count($tokens))->toBe(31);
+
+            return true;
+        })
+        ->assertViewHas('balances', function ($balances) {
+            expect(count($balances['prices']))->toBe(32);
+            expect(count($balances['totals']))->toBe(32);
+            expect(count($balances['ethereum']))->toBe(32);
+            expect(count($balances['prices_eur']))->toBe(32);
+            expect(count($balances['totals_eur']))->toBe(32);
+
+            return true;
+        });
 });
