66619944b0b97e3a6c1dcec93d113008fb5ee1b8
manel <manelgavalda1@gmail.com>
2023-12-15 16:36:47 +0100

    Rename test

--- resources/js/tests/dashboard.test.js
+++ resources/js/tests/dashboard.test.js
@@ -1,7 +1,7 @@
 import { it, expect } from 'vitest'
 import Dashboard from '../Pages/Dashboard.vue'
 
-it("testing currencyFormat", async () => {
+it("formats_the_number_as_a_currency", async () => {
   const currencyFormat = (number) => Dashboard.methods.currencyFormat(number)
 
   expect(currencyFormat(32121.123)).toBe("32,121.123");
